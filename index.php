a01462da8f0023e2a0e137d4270dc72fd2f515bf
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2024-04-11 16:15:14 +0200

    rest of functions and execute code

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!-------------------------------- HTML --------------------------------->
+
 <head>
     <title>My Finances</title>
     <style>
@@ -10,7 +12,7 @@
 
 <body>
     <h1>My Finances</h1>
-    <form name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <form name="form1" method="post">
 
         <p>Income/Expense<br>
             <select name="incexp" size="1" id="">
@@ -38,7 +40,7 @@
         <p>Amount <br>
             <input type="text" name="amount" size="20">
         </p>
-        <p><input type="submit" name="submitme" value="Add Transacion"></p>
+        <p><input type="submit" name="submitme" value="Add Transaction"></p>
     </form>
 
     <hr>
@@ -48,94 +50,167 @@
             <th>Inc/Exp</th>
             <th>Item</th>
             <th>Category</th>
-            <th>Amuount</th>
+            <th>Amount</th>
         </tr>
+
+
+        <?php
+        //------------------------------ Interfaces ----------------------------------------
+        
+        // Interfaces contains constant with delimited string and an abstract function
+        interface IncomeCategories
+        {
+            const INCOMECATS = "Stock*Paycheck";
+
+            public function checkIncomeCategory($category);
+        }
+
+        interface ExpenseCategories
+        {
+            const EXPENSECATS = "Rent*Food*Gas";
+            public function checkExpenseCategory($category);
+        }
+
+        //------------------------------ "Grand daddy class" (abstract) ------------------------------
+        
+        abstract class Transaction
+        {
+            protected $item;
+            protected $category;
+            protected $amount;
+
+            public function __construct($cItem, $cCategory, $cAmount)
+            {
+                $this->item = $cItem;
+                $this->category = $cCategory;
+                $this->amount = $cAmount;
+            }
+            public static function displayTrans()
+            {
+                $filename = "transfile.txt";
+                if (file_exists($filename)) {
+                    $fp = fopen($filename, "r");
+                    while (true) {
+                        $line = fgets($fp);
+                        if (feof($fp)) {
+                            //print "<br><b>Program Ended Normally</b>";
+                            return; //exit the function
+                        }
+                        list($aIncExp, $aItem, $aCategory, $aAmount) = explode('*', $line);
+
+                        print "<tr>";
+                        print "<td>$aIncExp</td>";
+                        print "<td>$aItem</td>";
+                        print "<td>$aCategory</td>";
+                        print "<td>$aAmount</td>";
+                        print "</tr>";
+                    }
+                } else {
+                    if (isset($fp)) {
+                        fclose($fp);
+                    }
+                }
+            }
+            public function addTrans($aIncExp)
+            {
+                $tran = $aIncExp . "*" . $this->item . "*";
+                $tran .= $this->category . "*" . $this->amount . "*\n";
+
+                $filename = "transfile.txt";
+                $fpw = fopen($filename, "a");
+                fwrite($fpw, $tran);
+                fclose($fpw);
+            }
+        }
+
+        //----------------Classes that inherits Transaction----------------------
+        //Income Transaction
+        class IncomeTrans extends Transaction implements IncomeCategories
+        {
+            private $tranType = "Income";
+            //from abstract function in interface, REQUIRED to define function here below:
+            public function checkIncomeCategory($category)
+            {
+                $validCategories = explode('*', IncomeCategories::INCOMECATS);
+                $isValid = false;
+
+                foreach ($validCategories as $element) {
+                    if ($element == $category) {
+                        $isValid = true;
+                    }
+                }
+                return $isValid;
+
+            }
+        }
+        //Expense Transaction
+        
+        class ExpenseTrans extends Transaction implements ExpenseCategories
+        {
+            private $tranType = "Expense";
+            public function checkExpenseCategory($category)
+            {
+                $validCategories = explode('*', ExpenseCategories::EXPENSECATS);
+                $isValid = false;
+
+                foreach ($validCategories as $element) {
+                    if ($element == $category) {
+                        $isValid = true;
+                    }
+                }
+                return $isValid;
+            }
+        }
+        //----------------Execute Code--------------------------------------
+        
+        //Checking if button exists (form is fully instanciated)
+        if (isset($_POST["submitme"])) {
+            $button = $_POST["submitme"];
+            echo "Formuläret har skickats! ";
+        } else {
+            //if not make the button an empty space and all displayTrans to show data
+            echo "Formuläret har inte skickats än.";
+            $button = " ";
+            Transaction::displayTrans();
+        }
+        //If we find the button, execute the rest---------------------------
+        // set new variables to data from form
+        if ($button == "Add Transaction") {
+            $postIncExp = $_POST['incexp'];
+            $postItem = $_POST['item'];
+            $postCategory = $_POST['category'];
+            $postAmount = $_POST['amount'];
+
+            echo "Vald typ av transaktion: " . $postIncExp;
+            echo "Valt objekt: " . $postItem;
+            echo "Vald kategori: " . $postCategory;
+            echo "Belopp: " . $postAmount;
+
+            $isValid = false;
+
+            //Income, create new object and set isValid to true or false
+            if ($postIncExp == 'Income') {
+                $myTransaction = new IncomeTrans($postItem, $postCategory, $postAmount);
+                var_dump($myTransaction);
+                $isValid = $myTransaction->checkIncomeCategory($postCategory);
+
+            } else {
+                //Expense, create new object and set isValid to true or false
+                if ($postIncExp == 'Expense') {
+                    $myTransaction = new ExpenseTrans($postItem, $postCategory, $postAmount);
+                    $isValid = $myTransaction->checkExpenseCategory($postCategory);
+                } else {
+                    //if third option "-"
+                    print "<p><b>Please select 'Income' or 'Expense'</b></p>";
+                }
+            }
+            if ($isValid) {
+                $myTransaction->addTrans($postIncExp);
+            } else {
+                print "<p><b>$postCategory is NOT a valid category</b></p>";
+            }
+            Transaction::displayTrans();
+        }
+        ?>
     </table>
 </body>
-
-<?php
-//------------------------------ Interfaces ----------------------------------------
-
-// Interfaces contains constant with delimited string and an abstract function
-interface IncomeCategories
-{
-    const INCOMECATS = "Stock*Paycheck";
-
-    public function checkIncomeCategory($category);
-}
-
-interface ExpenseCategories
-{
-    const EXPENSECATS = "Rent*Food*Gas";
-    public function checkExpenseCategory($category);
-}
-
-//------------------------------ "Grand daddy class" (abstract) ----------------------------------------
-
-abstract class Transaction
-{
-    protected $item;
-    protected $category;
-    protected $amount;
-
-    public function __construct($cItem, $cCategory, $cAmount)
-    {
-        $this->item = $cItem;
-        $this->category = $cCategory;
-        $this->amount = $cAmount;
-    }
-    public static function displayTrans()
-    {
-        $filename = "transfile.txt";
-        if (file_exists($filename)) {
-            $fp = fopen($filename, "r");
-            while (true) {
-                $line = fgets($fp);
-                if (feof($fp)) {
-                    print "<br><b>Program Ended Normally</b>";
-                    return; //exit the function
-                }
-                list($aIncExp, $aItem, $aCategory, $aAmount) = explode('*', $line);
-
-                print "<tr>";
-                print "<td>$aIncExp</td>";
-                print "<td>$aItem</td>";
-                print "<td>$aCategory</td>";
-                print "<td>$aAmount</td>";
-                print "</tr>";
-                fclose($fp);
-            }
-        }
-    }
-    public function addTrans($aIncExp)
-    {
-        $tran = $aIncExp . "*" . $this->item . "*";
-        $tran .= $this->category . "*" . $this->amount . "*\n";
-
-        $filename = "transfile.txt";
-        $fpw = fopen($filename, "a");
-        fwrite($fpw, $tran);
-        fclose($fpw);
-    }
-}
-
-//----------------Classes that inherits Transaction----------------------
-class IncomeTran extends Transaction implements IncomeCategories
-{
-    private $tranType = "Income";
-    //from abstract function in interface, REQUIRED to define function here below:
-    public function checkIncomeCategory($category)
-    {
-        $validCategories = explode('*', self::INCOMECATS);
-        $isValid = false;
-
-        foreach ($validCategories as $element) {
-            if ($element == $category) {
-                $isValid = true;
-            }
-        }
-        return $isValid;
-
-    }
-}
-?>
